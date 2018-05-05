d7200b561199b7d910eccca2147f91dc3d9876c9
CrystalLove221 <thogue314271@protonmail.com>
2018-05-04 20:47:43 -0400

    Additional tests added

--- src/StreetNodesTest.java
+++ src/StreetNodesTest.java
@@ -21,32 +21,32 @@ public class StreetNodesTest extends TestCase {
         nodes1 = new ArrayList<Node<Point>>();
         nodes2 = new ArrayList<Node<Point>>();
         
-        Point pt = new Point(3, 2);
+        Point pt = new Point(2, 2);
         ArrayList<String> places = new ArrayList<String>();
         places.add("place");
         places.add("fouh");
-        Node<Point> node1 = new Node(pt, places);
+        Node<Point> node1 = new Node<Point>(pt, places);
         nodes1.add(node1);
         
-        pt = new Point(2, 2);
+        pt = new Point(3, 2);
         places = new ArrayList<String>();
         places.add("lala");
         places.add("fafa");
-        node1 = new Node(pt, places);
+        node1 = new Node<Point>(pt, places);
         nodes1.add(node1);
         
         pt = new Point(0, 2);
         places = new ArrayList<String>();
         places.add("la");
         places.add("fa");
-        node1 = new Node(pt, places);
+        node1 = new Node<Point>(pt, places);
         nodes2.add(node1);
         
-        pt = new Point(1, 2);
+        pt = new Point(0, 3);
         places = new ArrayList<String>();
         places.add("lo");
         places.add("fo");
-        node1 = new Node(pt, places);
+        node1 = new Node<Point>(pt, places);
         nodes2.add(node1);
         
     }
@@ -76,10 +76,29 @@ public class StreetNodesTest extends TestCase {
     }
     
     public void testCompareTo() {
-        
+        assertEquals(0, street1.compareTo(street1));
+        assertEquals(-1, street1.compareTo(street2));
+        assertEquals(1, street2.compareTo(street1));
+        assertEquals(-1, street1.compareTo(null));
     }
     
     public void testAddPoint() {
+        Point pt = new Point(0, 0);
+        ArrayList<String> places = new ArrayList<String>();
+        places.add("Infinito's");
+        Node<Point> node2 = new Node<Point>(pt, places);
         
+        street1.addPoint(node2);
+        
+        assertEquals("Fouh: (0,0) ", street1.toString());
+        
+        street1.addPoint(null);
+        assertEquals("Fouh: (0,0) ", street1.toString());
+    }
+    
+    public void testToString() {
+        street1.setLocations(nodes1);
+        
+        assertEquals("Fouh: (2,2) (3,2) ", street1.toString());
     }
 }
